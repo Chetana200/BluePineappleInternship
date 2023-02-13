2cdcdba5ccb1d746d75986c6f17d420003a47064
Chetana Agarwal <93917296+Chetana200@users.noreply.github.com>
2023-02-13 10:17:07 +0530

    Add files via upload

---
+++ javascript codes/WordFrequency.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var contents = fs.readFileSync('textfile.txt', 'utf8');
+var words = contents.split(" ");
+var wordCounts = {};
+
+for (var i = 0; i < words.length; i++) {
+  var word = words[i].toLowerCase();
+  if (wordCounts[word] === undefined) {
+    wordCounts[word] = 1;
+  } else {
+    wordCounts[word]++;
+  }
+}
+
+console.log(wordCounts);
